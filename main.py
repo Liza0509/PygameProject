34f9c5ab82e948ea4dff29581e45b2c78e57e876
Py5 <abasinaelizaveta9@gmail.com>
2025-02-01 18:16:17 +0300

    2

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
 import random
 
+items = [
+    {"name": "Лёгкий клинок", "type": "weapon", "effect": 2, "cost": 100},
+    {"name": "Тяжёлый меч", "type": "weapon", "effect": 5, "cost": 200},
+    {"name": "Эпический меч", "type": "weapon", "effect": 10, "cost": 500},
+    {"name": "Кожаная броня", "type": "armor", "effect": 10, "cost": 150},
+    {"name": "Стальная броня", "type": "armor", "effect": 25, "cost": 400},
+    {"name": "Мифриловая броня", "type": "armor", "effect": 50, "cost": 800},
+]
 # Инициализация Pygame
+weapon = ''
+armor = ''
 pygame.init()
 print(123)
 # Размеры окна и ячеек
@@ -78,8 +88,6 @@ def generate_level(level):
             spider_healths.append(10)
 
 
-
-
 # Здоровье, урон и инвентарь
 miner_health = 50
 miner_damage = 5
@@ -108,6 +116,45 @@ def is_adjacent(pos1, pos2):
     return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) == 1
 
 
+def draw_buy_window():
+    # Размеры окна
+    window_width = 300
+    window_height = 200
+    window_x = (WINDOW_SIZE - window_width) // 2
+    window_y = (WINDOW_SIZE - window_height) // 2
+
+    # Рисуем окно
+    pygame.draw.rect(screen, (200, 200, 200), (window_x, window_y, window_width, window_height))
+    pygame.draw.rect(screen, BLACK, (window_x, window_y, window_width, window_height), 2)
+
+    greeting_text = font.render("вот, что я могу предложить!", True, BLACK)
+    screen.blit(greeting_text, (window_x + 20, window_y + 20))
+
+    button_width = 260
+    button_height = 40
+    button_margin = 10
+    button_x = window_x + 20
+    button_y = window_y + 60  # Начальная позиция для первой кнопки
+
+    buttons = []  # Список для хранения кнопок
+
+    # Создаем и рисуем кнопки для каждого предмета
+    for item_name in items:
+        rect = pygame.Rect(button_x, button_y, button_width, button_height)
+        pygame.draw.rect(screen, (70, 130, 180), rect)  # Цвет кнопки
+
+        # Обычное отображение текста без font.render
+        text_surface = font.render(str(item_name), True, (255, 255, 255))  # Генерация текста в текстовой поверхности
+        screen.blit(text_surface, (rect.x + 10, rect.y + 10))  # Рисуем текст на кнопке с отступом
+
+        # Добавляем кнопку в список
+        buttons.append((rect, item_name))
+
+        button_y += button_height + button_margin  # Смещение для следующей кнопки
+
+    return buttons
+
+
 def draw_trade_window():
     # Размеры окна
     window_width = 300
@@ -129,54 +176,15 @@ def draw_trade_window():
     pygame.draw.rect(screen, GREEN, sell_button)
     screen.blit(sell_text, (sell_button.x + 20, sell_button.y + 10))
 
-    return sell_button
+    buy_text = font.render("купить", True, WHITE)
+    buy_button = pygame.Rect(window_x + 50, window_y + 160, 200, 40)
+    pygame.draw.rect(screen, GREEN, buy_button)
+    screen.blit(buy_text, (buy_button.x + 20, buy_button.y + 10))
 
-def trade_with_trader():
-    global miner_health, miner_damage, money
-
-    running_trade = True
-    while running_trade:
-        # Отображение интерфейса торговли
-        screen.fill(WHITE)
-        trader_text = font.render("Добро пожаловать в мою лавку!", True, BLACK)
-        screen.blit(trader_text, (10, 10))
-
-        # Кнопки для предметов
-        items = [
-            {"name": "Лёгкий клинок", "type": "weapon", "effect": 2, "cost": 10},
-            {"name": "Тяжёлый меч", "type": "weapon", "effect": 5, "cost": 20},
-            {"name": "Эпический меч", "type": "weapon", "effect": 10, "cost": 50},
-            {"name": "Кожаная броня", "type": "armor", "effect": 10, "cost": 15},
-            {"name": "Стальная броня", "type": "armor", "effect": 25, "cost": 40},
-            {"name": "Мифриловая броня", "type": "armor", "effect": 50, "cost": 80},
-        ]
-
-        y_offset = 50
-        for item in items:
-            item_text = font.render(f"{item['name']} - {item['cost']} монет", True, BLACK)
-            screen.blit(item_text, (10, y_offset))
-            y_offset += 30
-
-        # Обработка событий торговли
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:  # Закрыть окно торговли
-                    running_trade = False
-                elif event.key == pygame.K_1 and money >= items[0]["cost"]:  # Покупка лёгкого клинка
-                    miner_damage += items[0]["effect"]
-                    money -= items[0]["cost"]
-                    running_trade = False
-                elif event.key == pygame.K_2 and money >= items[1]["cost"]:  # Покупка тяжёлого меча
-                    miner_damage += items[1]["effect"]
-                    money -= items[1]["cost"]
-                    running_trade = False
-
-        pygame.display.flip()
+    return sell_button, buy_button
 
 
+f = False
 # Основной игровой цикл
 running = True
 while running:
@@ -185,13 +193,13 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             new_position = miner_position[:]
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_w:
                 new_position[1] -= 1
-            elif event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_s:
                 new_position[1] += 1
-            elif event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_a:
                 new_position[0] -= 1
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_d:
                 new_position[0] += 1
 
             # Проверка границ и наличия камней
@@ -275,7 +283,6 @@ while running:
                     level += 1
                     generate_level(level)
 
-
     # Заливка фона
     screen.fill(WHITE)
     # Рисуем сетку
@@ -307,12 +314,22 @@ while running:
         trading = True
 
     if trading:
-        sell_button = draw_trade_window()
+        if not f:
+            sell_button, buy_button = draw_trade_window()  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+        # if buy_button.collidepoint(mouse_x, mouse_y):
+        #     buttons = draw_buy_window()
         # Обработка клика мыши
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mouse_x, mouse_y = event.pos
-            if sell_button.collidepoint(mouse_x, mouse_y):
+            if buy_button.collidepoint(mouse_x, mouse_y):
+                f = True
+                continue
+                # for i in range (len(buttons)):
+                #     if buttons[i].collidepoint(mouse_x,mouse_y):
+                #         pass
+            if sell_button.collidepoint(mouse_x,
+                                        mouse_y):  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 # Продажа камней
                 money += miner_inventory * 2  # Продажа обычных камней
                 money += diamond_count * 50  # Продажа алмазов
@@ -325,29 +342,115 @@ while running:
                 ruby_count = 0
                 emerald_count = 0
                 trading = False  # Закрываем окно
+    if f:
+        buttons = draw_buy_window()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                print('vfdbhjnklmdfjdshv')
+                new_position = miner_position[:]
+                if event.key == pygame.K_w:
+                    new_position[1] -= 1
+                elif event.key == pygame.K_s:
+                    new_position[1] += 1
+                elif event.key == pygame.K_a:
+                    new_position[0] -= 1
+                elif event.key == pygame.K_d:
+                    new_position[0] += 1
+                    # Проверка границ и наличия камней
+                if (0 <= new_position[0] < GRID_SIZE and
+                        0 <= new_position[1] < GRID_SIZE and
+                        tuple(new_position) not in rock_positions):
+                    f = False
+                    miner_position = new_position
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_x, mouse_y = event.pos
+                for rect, item in buttons:
+                    if rect.collidepoint(mouse_x, mouse_y):
+                        #print(item['name'])
+                        if item['name'] == "Лёгкий клинок":
+                            if money <= 100:
+                                print('недостаточно денег')
+                            if weapon == "Лёгкий клинок":
+                                print(item['name'] + " уже куплен")
+                                break
+                            money -= 100
+                            weapon = "Лёгкий клинок"
+                            miner_damage=5 +2
+                            print('buy ' + item['name'], weapon)
+                        elif item['name'] == "Тяжёлый меч":
+                            if money <= 200:
+                                print('недостаточно денег')
+                            if weapon == "Тяжёлый меч":
+                                print(item['name'] + " уже куплен")
+                                break
+                            money -= 200
+                            weapon = "Тяжёлый меч"
+                            miner_damage=5 + 5
+                            print('buy ' + item['name'], weapon)
+                        elif item['name'] == "Эпический меч":
+                            if money <= 500:
+                                print('недостаточно денег')
+                            if weapon == "Эпический меч":
+                                print(item['name'] + " уже куплен")
+                                break
+                            money -= 500
+                            weapon = "Эпический меч"
+                            miner_damage=5 + 10
+                            print('buy ' + item['name'], weapon)
+                        elif item['name'] == "Кожаная броня":
+                            if money <= 150:
+                                print('недостаточно денег')
+                            if armor == "Кожаная броня":
+                                print(item['name'] + " уже куплен")
+                                break
+                            money -= 150
+                            armor = "Кожаная броня"
+                            miner_health=50 + 10
+                            print('buy ' + item['name'], armor)
+                        elif item['name'] == "Стальная броня":
+                            if money <= 400:
+                                print('недостаточно денег')
+                            if armor == "Стальная броня":
+                                print(item['name'] + " уже куплен")
+                                break
+                            money -= 400
+                            armor = "Стальная броня"
+                            miner_health=50 + 25
+                            print('buy ' + item['name'], armor)
+                        elif item['name'] == "Мифриловая броня":
+                            if money <= 800:
+                                print('недостаточно денег')
+                            if armor == "Мифриловая броня":
+                                print(item['name'] + " уже куплен")
+                                break
+                            money -= 800
+                            armor = "Мифриловая броня"
+                            miner_health = 50 + 50
+                            print('buy ' + item['name'], armor)
+                        break
 
     # Отображение здоровья шахтёра
     miner_health_text = font.render(f"Шахтёр: {miner_health} HP", True, GREEN)
-    screen.blit(miner_health_text, (10, WINDOW_SIZE + 10))
+    screen.blit(miner_health_text, (100, WINDOW_SIZE + 10))
     # Отображение здоровья пауков
     for i, spider_position in enumerate(spider_positions):
         spider_health_text = font.render(f"Паук {i + 1}: {spider_healths[i]} HP", True, RED)
         screen.blit(spider_health_text, (WINDOW_SIZE - 150, 10 + i * 20))
-    # Отображение инвентаря
+    # Отображение
     inventory_text = font.render(f"Камни: {miner_inventory}", True, BLACK)
     screen.blit(inventory_text, (10, WINDOW_SIZE + 40))
-    # Отображение денег
     money_text = font.render(f"Монеты: {money}", True, BLACK)
-    screen.blit(money_text, (10, WINDOW_SIZE + 100))
+    screen.blit(money_text, (100, WINDOW_SIZE + 100))
+    damage_text = font.render(f"Урон: {miner_damage}", True, RED)
+    screen.blit(damage_text, (100, WINDOW_SIZE + 50))
 
-    # Отображение драгоценных камней
     diamond_text = font.render(f"Алмазы: {diamond_count}", True, (0, 255, 255))  # Голубой
-    ruby_text = font.render(f"Рубины: {ruby_count}", True, (255, 0, 0))  # Красный
-    emerald_text = font.render(f"Изумруды: {emerald_count}", True, (0, 255, 0))  # Зелёный
-
-    # Рисуем текст для каждого типа
     screen.blit(diamond_text, (10, WINDOW_SIZE + 70))
+    ruby_text = font.render(f"Рубины: {ruby_count}", True, (255, 0, 0))  # Красный
     screen.blit(ruby_text, (10, WINDOW_SIZE + 100))
+    emerald_text = font.render(f"Изумруды: {emerald_count}", True, (0, 255, 0))  # Зелёный
     screen.blit(emerald_text, (10, WINDOW_SIZE + 130))
     # Обновление экрана
     pygame.display.flip()
